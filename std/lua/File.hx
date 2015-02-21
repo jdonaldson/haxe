5cf179c7aec61520e09fe41686bc5b10c2f7442e
PeyTy <alegorium@gmail.com>
2015-02-21 03:06:50 +0300

    File

---
+++ std/lua/File.hx
@@ -0,0 +1,12 @@
+package lua;
+
+extern class File
+{
+	function close():Void;
+	function flush():Void;
+	function lines():Dynamic; // to-do?
+	function read(?format:Dynamic):String;
+	function seek(?whence:String, ?offset:Int):Dynamic;
+	function setvbuf(mode:String, ?size:Int):Void;
+	function write(d:Dynamic):Void;
+}
