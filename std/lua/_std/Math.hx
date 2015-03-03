2adf815b733916f38f985059c18c1009737da850
PeyTy <alegorium@gmail.com>
2015-03-03 04:14:25 +0300

    Math

--- std/lua/_std/Math.hx
+++ std/lua/_std/Math.hx
@@ -23,66 +23,114 @@ package;
 
 // Can't enable @:coreApi because some fields are now inline getters
 // @:coreApi
-@:keepInit
-extern class Math
+@: keepInit
+class Math
 {
-	static var PI(default,null) : Float;
+	public static var PI(default, null) : Float = untyped math.pi;
 
-	static var NEGATIVE_INFINITY(get, null) : Float;
-	private static inline function get_NEGATIVE_INFINITY () : Float {
-		return -(untyped __js__("Infinity"));
+	public static var NEGATIVE_INFINITY(get, null) : Float;
+	private static inline function get_NEGATIVE_INFINITY () : Float
+	{
+		return untyped __lua__("(-1.0 / 0.0)");
 	}
 
-	static var POSITIVE_INFINITY(get,null) : Float;
-	private static inline function get_POSITIVE_INFINITY () : Float {
-		return (untyped __js__("Infinity"));
+	public static var POSITIVE_INFINITY(get, null) : Float;
+	private static inline function get_POSITIVE_INFINITY () : Float
+	{
+		return untyped __lua__("(1.0 / 0.0)");
 	}
 
-	static var NaN(get, null) : Float;
-	private static inline function get_NaN () : Float {
-		return (untyped __js__("NaN"));
+	public static var NaN(get, null) : Float;
+	private static inline function get_NaN () : Float
+	{
+		return untyped __lua__("(0 / 0)");
 	}
 
-	static function abs(v:Float):Float;
-	static function acos(v:Float):Float;
-	static function asin(v:Float):Float;
-	static function atan(v:Float):Float;
-	static function atan2(y:Float, x:Float):Float;
-	static function ceil(v:Float):Int;
-	static function cos(v:Float):Float;
-	static function exp(v:Float):Float;
-	static function floor(v:Float):Int;
-	static function log(v:Float):Float;
-	static function max(a:Float, b:Float):Float;
-	static function min(a:Float, b:Float):Float;
-	static function pow(v:Float, exp:Float):Float;
-	static function random() : Float;
-	static function round(v:Float):Int;
-	static function sin(v:Float):Float;
-	static function sqrt(v:Float):Float;
-	static function tan(v:Float):Float;
+	public static inline function abs(v: Float): Float
+	return untyped __global__(math.abs, v);
 
-	static inline function ffloor( v : Float ) : Float {
+	public static inline function acos(v: Float): Float
+	return untyped __global__(math.acos, v);
+
+	public static inline function asin(v: Float): Float
+	return untyped __global__(math.asin, v);
+
+	public static inline function atan(v: Float): Float
+	return untyped __global__(math.atan, v);
+
+	public static inline function atan2(y: Float, x: Float): Float
+	return untyped __global__(math.atan2, y, x);
+
+	public static inline function ceil(v: Float): Int
+	return round(untyped __global__(math.ceil, v));
+
+	public static inline function cos(v: Float): Float
+	return untyped __global__(math.cos, v);
+
+	public static inline function exp(v: Float): Float
+	return untyped __global__(math.exp, v);
+
+	public static inline function floor(v: Float): Int
+	return round(untyped __global__(math.floor, v));
+
+	public static inline function log(v: Float): Float
+	return untyped __global__(math.log, v);
+
+	public static inline function max(a: Float, b: Float): Float
+	return untyped __global__(math.max, a);
+
+	public static inline function min(a: Float, b: Float): Float
+	return untyped __global__(math.min, a);
+
+	public static inline function pow(v: Float, exp: Float): Float
+	return untyped __global__(math.pow, v, exp);
+
+	public static inline function random() : Float
+	return untyped __global__(math.random);
+
+	public static function round(v: Float): Int
+	{
+		if (v >= 0)
+			return untyped __global__(math.floor, v + 0.5)
+			else return untyped __global__(math.ceil, v - 0.5);
+	}
+
+	public static inline function sin(v: Float): Float
+	return untyped __global__(math.sin, v);
+
+	public static inline function sqrt(v: Float): Float
+	return untyped __global__(math.sqrt, v);
+
+	public static inline function tan(v: Float): Float
+	return untyped __global__(math.tan, v);
+
+	public static inline function ffloor( v : Float ) : Float
+	{
 		return floor(v);
 	}
 
-	static inline function fceil( v : Float ) : Float {
+	public static inline function fceil( v : Float ) : Float
+	{
 		return ceil(v);
 	}
 
-	static inline function fround( v : Float ) : Float {
+	public static inline function fround( v : Float ) : Float
+	{
 		return round(v);
 	}
 
-	static inline function isFinite( f : Float ) : Bool {
-		return (untyped __js__("isFinite"))(f);
+	public static inline function isFinite( f : Float ) : Bool
+	{
+		return return f != NEGATIVE_INFINITY && f != POSITIVE_INFINITY && f != NaN;
 	}
 
-	static inline function isNaN( f : Float ) : Bool {
-		return (untyped __js__("isNaN"))(f);
+	public static inline function isNaN( f : Float ) : Bool
+	{
+		return f == NaN;
 	}
 
-	static function __init__() : Void {
+	public static function __init__() : Void
+	{
 		untyped __feature__("Type.resolveClass", $hxClasses["Math"] = Math);
 	}
 }
